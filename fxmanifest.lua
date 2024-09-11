17c9491c4320be8d97aa957bd6a107256d7a0f18
Viera Wattson <126415488+V1ERA@users.noreply.github.com>
2024-09-11 12:07:10 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,7 @@
--- lua54 "yes"
--- client_script '@F-AC/antiInjection/client.lua'
--- server_script '@F-AC/antiInjection/server.lua'
-
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Cute Cat'
+author 'Viera'
 description 'Lua executor with custom NUI'
 version '1.0.0'
 
